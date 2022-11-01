8298a6d576e786935c2fd1ca2c31d8c465fff9e1
SUjay <sujaysavanthd4@gmail.com>
2022-11-01 13:56:03 +0530

    Added Components and updated the _app.js with different routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../component';
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  // Theme will change with attribute class
+  <ThemeProvider attribute="class">
+    <div className="bg-white min-h-screen dark:bg-nft-dark">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/e1adcde454.js" crossorigin="anonymous" />
+  </ThemeProvider>
+
+);
+export default MyApp;
