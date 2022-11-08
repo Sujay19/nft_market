0e2de0268e96fd78e2a8e9420e89c788fadff158
SUjay <sujaysavanthd4@gmail.com>
2022-11-08 10:19:23 +0530

    Updated Foooter component with content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
-const Footer = () => (
-  <div>Footer</div>
+import images from '../assets';
+// eslint-disable-next-line import/no-cycle
+import { Button } from './index.js';
+
+const FooterLinks = ({ heading, items }) => (
+  <div className="flex-1  justify-start items-center">
+    <h3 className=" font-poppins font-semibold dark:text-white text-nft-black-1 text-xl mb-10">
+      {heading}
+    </h3>
+    {
+    items.map((item, index) => (
+      <p key={index} className="font-poppins  dark:text-white text-nft-black-1 my-3 dark:hover:text-nft-gray-1 hover:text-nft-black-1 cursor-pointer">{item}</p>
+    ))
+}
+  </div>
 );
+
+const Footer = () => {
+  const { theme } = useTheme();
+  return (
+    <footer className="flexCenter flex-col border-t  dark:border-nft-black-1 border-nft-gray-1 py-16 sm:py-8 ">
+      <div className="flex flex-row w-full  minmd:w-4/5 md:flex-col sm:px-4 px-16">
+        <div className="flexStart flex-1 flex-col">
+          <div className="flexCenter  cursor-pointer ">
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              height={32}
+              width={32}
+              alt="logo2"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold  text-lg  ml-1 ">CryptoKet</p>
+          </div>
+          <p className="text-base font-poppins font-semibold dark:text-white text-nft-black-1 mt-6  ">Get the latest updates</p>
+          <div className="flexBetween flex-row md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border  dark:border-nft-black-2 border-nft-gray-2  rounded-md">
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="h-full flex-1 w-full rounded-md dark:bg-nft-black-2 bg-white px-4  dark:text-white text-black font-normal text-sm  minlg:text-lg outline-none"
+            />
+            <div className="flex-initial">
+              <Button btnName="Email me" classStyles="rounded-md" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:mt-8 md:ml-0">
+          <FooterLinks heading="Cryptoket" items={['Explore', 'How it works', 'Contact Us']} />
+          <FooterLinks heading="Support" items={['Help Center', 'Terms of service', 'Legal', 'Privacy Policy']} />
+        </div>
+      </div>
+      <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
+        <div className=" flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
+          <p className=" font-poppins dark:text-white text-nft-black-1 font-semibold text-base ">CryptoKet,Inc. All Rights Reserved</p>
+          <div className="flex flex-row sm:mt-4">
+            {[images.instagram, images.twitter, images.telegram, images.discord].map((image, index) => (
+              <div className="mx-2 cursor-pointer " key={index}>
+                <Image
+                  src={image}
+                  objectFit="contain"
+                  height={24}
+                  width={24}
+                  alt="Socials"
+                  className={theme === 'light' && 'filter invert'}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
