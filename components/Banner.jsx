5306264ca53e52783a80e49f7273bdbc5d1fc85d
SUjay <sujaysavanthd4@gmail.com>
2022-11-08 13:32:11 +0530

    added Banner js file and updated home page

---
+++ components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = ({ name, parentStyles, childStyles }) => (
+  <div className={`w-full relative flex items-center  overflow-hidden  z-0 nft-gradient ${parentStyles}`}>
+    <p className={`font-poppins font-bold leading-70 text-white text-5xl ${childStyles} `}> {name}</p>
+    <div className="absolute w-48 h-48 sm:w-32 sm:h-32 -top-9 -left-16 rounded-full  white-bg  -z-5" />
+    <div className="absolute w-72 h-72 sm:w-56 sm:h-56 -bottom-24 -right-14 rounded-full  white-bg -z-5" />
+  </div>
+);
+
+export default Banner;
+
